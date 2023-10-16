1ee1cd39db0183de6655122ff23612af8f98c8d4
Bruno Barrios <bbarriosvargas-external@etsglobal.org>
2023-10-16 19:51:07 -0400

    🚧: added crud tests for board

--- trello-api/src/test/java/com/trello/BoardTest.java
+++ trello-api/src/test/java/com/trello/BoardTest.java
@@ -6,19 +6,37 @@ package com.trello;
 
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 
 public class BoardTest {
+    private RequestSpecification requestSpec;
+    private ResponseSpecification responseSpec;
+
+    @BeforeClass
+    public void setUp() {
+        requestSpec = new RequestSpecBuilder().setBaseUri("https://api.trello.com/1").build();
+        responseSpec = new ResponseSpecBuilder().expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
     @Test
     public void testCreateBoard() {
         //Arrange
-        String endpoint =  "https://api.trello.com/1/boards/";
-        String apiKey = "";
-        String apiToken = "";
-        String boardName = "bruno test board";
+//        String endpoint =  "https://api.trello.com/1/boards/";
+        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
+//        String apiKey = "";
+        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
+        String boardName = "bruno test board 1 to test body";
 //        String endpoint = baseUrl + "?name="+ boardName + "&key=" + apiKey + "&token=" +apiToken;
 
 
@@ -31,11 +49,125 @@ public class BoardTest {
         queryParams.put("name", boardName);
 
         //Act
-        var response = RestAssured.given().log().all().when()
+        var response = RestAssured.given()
+                .spec(requestSpec).log().all().when()
                 .headers(headers)
                 .queryParams(queryParams)
-                        .post(endpoint);
+                        .post("/boards/")
+                .then()
+                .spec(responseSpec).extract().response();
+        //Assert
+//        Assert.assertEquals(response.statusCode(), 200);
+        System.out.println(response.getBody().asPrettyString());
+    }
+
+    @Test
+    public void testCreateBoardReqSpec() {
+        //Arrange
+
+//        String endpoint =  "https://api.trello.com/1/boards/";
+        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
+        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
+        String boardName = "bruno test board 1-2";
+//        String endpoint = baseUrl + "?name="+ boardName + "&key=" + apiKey + "&token=" +apiToken;
+
+
+        var headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+
+        var queryParams = new HashMap<String, String>();
+        queryParams.put("key", apiKey);
+        queryParams.put("token", apiToken);
+        queryParams.put("name", boardName);
+
+        //Act
+        var response = RestAssured.given()
+                .spec(requestSpec)
+                .log().all().when()
+                .headers(headers)
+                .queryParams(queryParams)
+                        .post("/boards/");
+
+        System.out.println(response.getBody().asPrettyString());
         //Assert
         Assert.assertEquals(response.statusCode(), 200);
+    }
+
+    @Test
+    public void UpdateBoard() {
+        //AAA
+        //Arrange
+        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
+        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
+        String boardName = "bruno test board 1-2 Updated";
+
+
+        var headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+
+        var queryParams = new HashMap<String, String>();
+        queryParams.put("key", apiKey);
+        queryParams.put("token", apiToken);
+        queryParams.put("name", boardName);
+
+        //Act
+        var response = RestAssured.given()
+                .spec(requestSpec)
+                .log().all().when()
+                .headers(headers)
+                .queryParams(queryParams)
+                .put("/boards/652dc67e5cc4f63a15172cea");
+        System.out.println(response.getBody().asPrettyString());
+        //Asserts
+        Assert.assertEquals(response.statusCode(), 200);
+    }
+
+    @Test
+    public void getBoardTest() {
+        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
+//        String apiKey = "";
+        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
+
+        var headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+
+        var queryParams = new HashMap<String, String>();
+        queryParams.put("key", apiKey);
+        queryParams.put("token", apiToken);
+
+        var response = RestAssured.given()
+                .spec(requestSpec)
+                .log().all().when()
+                .headers(headers)
+                .queryParams(queryParams)
+                .get("/boards/652dc9d80d310150df15b7cf").then()
+                .spec(responseSpec).extract().response();
+
+        String name = response.path("name");
+
+        Assert.assertEquals(name, "bruno test board 1 to test body" );
+    }
+
+    @Test
+    public void deleteBoardTest() {
+        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
+        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
+
+        var headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+
+        var queryParams = new HashMap<String, String>();
+        queryParams.put("key", apiKey);
+        queryParams.put("token", apiToken);
+
+        var response = RestAssured.given()
+                .spec(requestSpec)
+                .log().all().when()
+                .headers(headers)
+                .queryParams(queryParams)
+                .delete("/boards/652dc9d80d310150df15b7cf").then()
+                .spec(responseSpec).extract().response();
+        System.out.println("Board Deleted");
+        System.out.println(response.getBody().asPrettyString());
     }
 }
