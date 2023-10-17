68d95f562c7140c461dcc578391484b2c29a02d4
Bruno Barrios <bbarriosvargas-external@etsglobal.org>
2023-10-16 20:17:30 -0400

    🚧: refactor crud board

--- trello-api/src/test/java/com/trello/BoardTest.java
+++ trello-api/src/test/java/com/trello/BoardTest.java
@@ -1,10 +1,5 @@
 package com.trello;
 
-//curl --location --request POST 'https://api.trello.com/1/boards/?name=AT-08-class-3f1c13d1-f4cd-4377-95ed-f4f7a12320d5&key=&token=' \
-
-
-
-
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
@@ -13,71 +8,65 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class BoardTest {
     private RequestSpecification requestSpec;
     private ResponseSpecification responseSpec;
+    private String apiKey;
+    private String apiToken;
+    private Map<String, String> headers;
+    private Map<String, String> queryParams;
+
+    private String boardID;
 
     @BeforeClass
     public void setUp() {
+        apiKey = "84b4afa69fcbbb3e12ece30db732262a";
+        apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
         requestSpec = new RequestSpecBuilder().setBaseUri("https://api.trello.com/1").build();
         responseSpec = new ResponseSpecBuilder().expectStatusCode(200)
                 .expectContentType(ContentType.JSON)
                 .build();
-    }
-    @Test
-    public void testCreateBoard() {
-        //Arrange
-//        String endpoint =  "https://api.trello.com/1/boards/";
-        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
-//        String apiKey = "";
-        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
-        String boardName = "bruno test board 1 to test body";
-//        String endpoint = baseUrl + "?name="+ boardName + "&key=" + apiKey + "&token=" +apiToken;
 
-
-        var headers = new HashMap<String, String>();
+        headers = new HashMap<String, String>();
         headers.put("Content-Type", "application/json");
 
-        var queryParams = new HashMap<String, String>();
+        queryParams = new HashMap<String, String>();
         queryParams.put("key", apiKey);
         queryParams.put("token", apiToken);
-        queryParams.put("name", boardName);
-
-        //Act
-        var response = RestAssured.given()
-                .spec(requestSpec).log().all().when()
-                .headers(headers)
-                .queryParams(queryParams)
-                        .post("/boards/")
-                .then()
-                .spec(responseSpec).extract().response();
-        //Assert
-//        Assert.assertEquals(response.statusCode(), 200);
-        System.out.println(response.getBody().asPrettyString());
     }
 
-    @Test
+//    @Test
+//    public void testCreateBoard() {
+//        //Arrange
+//        String boardName = "bruno test board 1 to test body";
+//
+//        queryParams.put("name", boardName);
+//
+//        //Act
+//        var response = RestAssured.given()
+//                .spec(requestSpec).log().all().when()
+//                .headers(headers)
+//                .queryParams(queryParams)
+//                .post("/boards/")
+//                .then()
+//                .spec(responseSpec).extract().response();
+//        //Assert
+//        System.out.println(response.getBody().asPrettyString());
+//    }
+
+    @Test(priority = 1)
     public void testCreateBoardReqSpec() {
         //Arrange
 
-//        String endpoint =  "https://api.trello.com/1/boards/";
-        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
-        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
         String boardName = "bruno test board 1-2";
-//        String endpoint = baseUrl + "?name="+ boardName + "&key=" + apiKey + "&token=" +apiToken;
 
-
-        var headers = new HashMap<String, String>();
-        headers.put("Content-Type", "application/json");
-
-        var queryParams = new HashMap<String, String>();
-        queryParams.put("key", apiKey);
-        queryParams.put("token", apiToken);
         queryParams.put("name", boardName);
 
         //Act
@@ -86,28 +75,22 @@ public class BoardTest {
                 .log().all().when()
                 .headers(headers)
                 .queryParams(queryParams)
-                        .post("/boards/");
+                .post("/boards/");
 
         System.out.println(response.getBody().asPrettyString());
         //Assert
         Assert.assertEquals(response.statusCode(), 200);
+
+        boardID = response.getBody().path("id");
+        System.out.println(String.format("boardID: %s", boardID));
     }
 
-    @Test
+    @Test(priority = 2)
     public void UpdateBoard() {
         //AAA
         //Arrange
-        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
-        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
         String boardName = "bruno test board 1-2 Updated";
 
-
-        var headers = new HashMap<String, String>();
-        headers.put("Content-Type", "application/json");
-
-        var queryParams = new HashMap<String, String>();
-        queryParams.put("key", apiKey);
-        queryParams.put("token", apiToken);
         queryParams.put("name", boardName);
 
         //Act
@@ -116,56 +99,36 @@ public class BoardTest {
                 .log().all().when()
                 .headers(headers)
                 .queryParams(queryParams)
-                .put("/boards/652dc67e5cc4f63a15172cea");
+                .put(String.format("/boards/%s", boardID));
         System.out.println(response.getBody().asPrettyString());
         //Asserts
         Assert.assertEquals(response.statusCode(), 200);
     }
 
-    @Test
+    @Test(priority = 3)
     public void getBoardTest() {
-        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
-//        String apiKey = "";
-        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
-
-        var headers = new HashMap<String, String>();
-        headers.put("Content-Type", "application/json");
-
-        var queryParams = new HashMap<String, String>();
-        queryParams.put("key", apiKey);
-        queryParams.put("token", apiToken);
 
         var response = RestAssured.given()
                 .spec(requestSpec)
                 .log().all().when()
                 .headers(headers)
                 .queryParams(queryParams)
-                .get("/boards/652dc9d80d310150df15b7cf").then()
+                .get(String.format("/boards/%s", boardID)).then()
                 .spec(responseSpec).extract().response();
 
         String name = response.path("name");
 
-        Assert.assertEquals(name, "bruno test board 1 to test body" );
+        Assert.assertEquals(name, "bruno test board 1-2 Updated");
     }
 
-    @Test
+    @Test(priority = 4)
     public void deleteBoardTest() {
-        String apiKey = "84b4afa69fcbbb3e12ece30db732262a";
-        String apiToken = "ATTA0c333e3feb33cfb68bf5cc2528a45d1bc620f47a2568c5e084412a46fa536bd10CF076E7";
-
-        var headers = new HashMap<String, String>();
-        headers.put("Content-Type", "application/json");
-
-        var queryParams = new HashMap<String, String>();
-        queryParams.put("key", apiKey);
-        queryParams.put("token", apiToken);
-
         var response = RestAssured.given()
                 .spec(requestSpec)
                 .log().all().when()
                 .headers(headers)
                 .queryParams(queryParams)
-                .delete("/boards/652dc9d80d310150df15b7cf").then()
+                .delete(String.format("/boards/%s", boardID)).then()
                 .spec(responseSpec).extract().response();
         System.out.println("Board Deleted");
         System.out.println(response.getBody().asPrettyString());
