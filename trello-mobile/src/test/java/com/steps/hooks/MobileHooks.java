659a096b83d3862ce9cb666f3c7e6f1ee951db41
Bruno Barrios <sperbru797@gmail.com>
2023-11-22 10:26:24 -0400

    🔨 fixed hook to not update browserstack status when running locally

--- trello-mobile/src/test/java/com/steps/hooks/MobileHooks.java
+++ trello-mobile/src/test/java/com/steps/hooks/MobileHooks.java
@@ -1,6 +1,8 @@
 package com.steps.hooks;
 
 import com.appium.DriverManager;
+import com.appium.Mobile;
+import com.trello.utils.PropertiesInfo;
 import com.utils.AppiumCommonActions;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
@@ -15,13 +17,17 @@ public class MobileHooks {
     @After
     public void tearDown(Scenario scenario) {
         LOGGER.info("After hook for scenario: " + scenario.getName());
+        String browserStackStatusMessage = "";
         if (scenario.isFailed()) {
-            AppiumCommonActions.runJSEScript("browserstack_executor: {\"action\": \"setSessionStatus\",\"arguments\": {\"status\": \"failed\", \"reason\": \"TEST FAILED!\"}}");
+            browserStackStatusMessage = "browserstack_executor: {\"action\": \"setSessionStatus\",\"arguments\": {\"status\": \"failed\", \"reason\": \"TEST FAILED!\"}}";
             LOGGER.info(scenario.getName() + " has failed");
         } else {
-            // set test status as pass
-            AppiumCommonActions.runJSEScript("browserstack_executor: {\"action\": \"setSessionStatus\",\"arguments\": {\"status\": \"passed\", \"reason\": \"TEST PASSED!\"}}");
+            browserStackStatusMessage = "browserstack_executor: {\"action\": \"setSessionStatus\",\"arguments\": {\"status\": \"passed\", \"reason\": \"TEST PASSED!\"}}";
             LOGGER.info(scenario.getName() + " has passed");
+        }
+
+        if (PropertiesInfo.getInstance().getEnv().equalsIgnoreCase(Mobile.Android_BrowserStack.toString())) {
+            AppiumCommonActions.runJSEScript(browserStackStatusMessage);
         }
         // cast to JavascriptExecutor
 
